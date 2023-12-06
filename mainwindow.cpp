2d85129f74b6ac17b4a2f44b8c269f83adcf91fb
oyyq0390 <153047647+oyyq0390@users.noreply.github.com>
2023-12-06 11:20:30 +0800

    mainwindow.ui
    加了订单信息的界面设计

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,7 +11,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    loadData("D://College//QTProject//zuoye//jipiaoshuju.txt");
+    loadData("D://QT//QT projects//keshe//AirTicketCHD//jipiaoshuju.txt");
 }
 
 MainWindow::~MainWindow()
